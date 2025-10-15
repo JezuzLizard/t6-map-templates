7fedc16e4e62c1a82e57e5a35c33b64a9318ff66
EvelynYuki <195348040+EvelynYuki@users.noreply.github.com>
2025-10-14 22:35:49 -0400

    move compiled fastfiles to zone_out and copy to mods folder

--- zm_templates/build.bat
+++ zm_templates/build.bat
@@ -6,23 +6,25 @@ set "COMMON_PATH=%cd%\common"
 set "SOURCE_PATH=%cd%\%MOD_NAME%\zone_source"
 set "SOURCE_PATH_TEMPLATED=%cd%\common\zone_source"
 set "MAP_BASE=%cd%\%MOD_NAME%"
+set "ZONE_OUT=%MAP_BASE%\zone_out"
 
 if not exist "%OAT_BASE%" (
-    echo Downloading OpenAssetTools...
+    echo Downloading OAT...
     powershell -Command "Invoke-WebRequest -Uri https://github.com/Laupetin/OpenAssetTools/releases/latest/download/oat-windows.zip -OutFile oat-windows.zip"
     
-    echo Extracting OpenAssetTools...
+    echo Extracting OAT...
     powershell -Command "Expand-Archive -Path oat-windows.zip -DestinationPath '%OAT_BASE%' -Force"
     
     del oat-windows.zip
     
-    echo OpenAssetTools has been successfully downloaded and extracted.
+    echo OAT has been successfully downloaded and extracted.
+    echo.
+    echo.
     echo.
 )
 
 :: frontend.ff, have to remove the soundbank
 "%OAT_BASE%\Linker.exe" ^
--v ^
 --load "%BIN_PATH%\common.ff" ^
 --load "%BIN_PATH%\zm_transit.ff" ^
 --load "%BIN_PATH%\zm_transit_patch.ff" ^
@@ -33,21 +35,27 @@ if not exist "%OAT_BASE%" (
 --add-asset-search-path "%MAP_BASE%" ^
 --add-source-search-path "%SOURCE_PATH%" ^
 --add-source-search-path "%SOURCE_PATH_TEMPLATED%" ^
---output-folder "%MAP_BASE%" frontend
+--output-folder "%ZONE_OUT%" frontend
+
+echo.
+echo.
+echo.
 
 :: en_frontend.ff, have to remove the soundbank
 "%OAT_BASE%\Linker.exe" ^
--v ^
 --base-folder "%MAP_BASE%\en_frontend" ^
 --add-asset-search-path "%COMMON_PATH%" ^
 --add-asset-search-path "%MAP_BASE%\en_frontend" ^
 --add-source-search-path "%SOURCE_PATH%" ^
 --add-source-search-path "%SOURCE_PATH_TEMPLATED%" ^
---output-folder "%MAP_BASE%" en_frontend
+--output-folder "%ZONE_OUT%" en_frontend
+
+echo.
+echo.
+echo.
 
 :: mod.ff
 "%OAT_BASE%\Linker.exe" ^
--v ^
 --load "%BIN_PATH%\common.ff" ^
 --load "%BIN_PATH%\zm_transit.ff" ^
 --load "%BIN_PATH%\zm_transit_patch.ff" ^
@@ -57,4 +65,21 @@ if not exist "%OAT_BASE%" (
 --add-asset-search-path "%MAP_BASE%" ^
 --add-source-search-path "%SOURCE_PATH%" ^
 --add-source-search-path "%SOURCE_PATH_TEMPLATED%" ^
---output-folder "%MAP_BASE%" mod
+--output-folder "%ZONE_OUT%" mod
+
+echo.
+echo.
+echo.
+
+:: copy to the mods folder
+set err=%ERRORLEVEL%
+
+if %err% EQU 0 (
+    XCOPY "%ZONE_OUT%" "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%" /Y /D /H /K /I
+) ELSE (
+    COLOR C
+    echo FAILED!
+)
+
+echo Compiled mod moved to "%LOCALAPPDATA%\Plutonium\storage\t6\mods\%MOD_NAME%".
+pause
